1af2af4a3e4f3de231414c51299cefa604d3dcbd
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 04:14:20 -0700

    Outline for shape.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Modules
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shape.js");
 
 // Questions for user
 const questions = [{}];
