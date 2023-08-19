1139e81f597e486bb1de844208b7b57f39d47dbf
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 03:52:43 -0700

    Outline for index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Questions for user
+const questions = [{}];
+
+class svg {
+  constructor() {}
+  render() {}
+}
+
+// Write README content to a file
+function writeToFile(fileName, data) {
+  fs.writeFile();
+}
+
+// Initialize the process
+function init() {
+  inquirer.prompt(questions).then().catch();
+}
