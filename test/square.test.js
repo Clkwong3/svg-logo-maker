f68841503ccf13f16906c9f0461950b1d0734f63
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 20:30:28 -0700

    forgot to adjust the values and capitalize Square

--- test/square.test.js
+++ test/square.test.js
@@ -5,10 +5,10 @@ describe("square Attributes", () => {
   // Test 1
   test("renders a square with CORRECT attributes", () => {
     // Create an instance of square with color yellow
-    const square = new square("yellow");
+    const square = new Square("yellow");
 
     // Define the expected SVG markup for a yellow square
-    const correctAttributes = `<rect x="50" y="50" width="200" height="200"  fill="yellow"/>`;
+    const correctAttributes = `<rect x="50" y="50" width="100" height="100" fill="yellow"/>`;
 
     // Check if the rendered SVG matches the expected SVG
     expect(square.render()).toBe(correctAttributes);
