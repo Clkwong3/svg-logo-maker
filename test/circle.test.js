e7395bd911ea8b2f21d83e1e1d3cc6db9f7e2ef5
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 20:13:51 -0700

    Second test for Circle works. Might rework both tests for radius, dimension and colors depending on input

--- test/circle.test.js
+++ test/circle.test.js
@@ -3,14 +3,26 @@ const { Circle } = require("../lib/shape");
 // describe block for Circle
 describe("Circle Attributes", () => {
   // Test 1
-  test("renders a circle with correct attributes", () => {
-    // Create an instance of Circle with color blue
+  test("renders a circle with CORRECT attributes", () => {
+    // Create an instance of Circle with color green
     const circle = new Circle("green");
 
-    // Define the expected SVG markup for a blue circle
-    const expectedSvgMarkup = `<circle cx="50" cy="50" r="100" width="100" height="100" fill="green"/>`;
+    // Define the expected SVG markup for a green circle
+    const correctAttributes = `<circle cx="50" cy="50" r="100" width="100" height="100" fill="green"/>`;
 
     // Check if the rendered SVG matches the expected SVG
-    expect(circle.render()).toBe(expectedSvgMarkup);
+    expect(circle.render()).toBe(correctAttributes);
+  });
+
+  // Test 2
+  test("renders a circle with INCORRECT attributes", () => {
+    // Create an instance of Circle with invalid attributes
+    const circle = new Circle("orange");
+
+    // Define the expected SVG markup for a orange circle
+    const incorrectAttributes = `<circle cx="50" cy="50" r="100" width="100" height="100" fill="orange"/>`;
+
+    // Check if the rendered SVG doesn't match the expected SVG
+    expect(circle.render()).toBe(incorrectAttributes);
   });
 });
