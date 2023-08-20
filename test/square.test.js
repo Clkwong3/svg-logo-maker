0f659ad6363d7e0da7205b7fc4f7eaff01493876
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-19 20:33:39 -0700

    Second test for Square runs and pass in terminal

--- test/square.test.js
+++ test/square.test.js
@@ -13,4 +13,19 @@ describe("square Attributes", () => {
     // Check if the rendered SVG matches the expected SVG
     expect(square.render()).toBe(correctAttributes);
   });
+
+  // describe block for square
+  describe("square Attributes", () => {
+    // Test 1
+    test("renders a square with INCORRECT attributes", () => {
+      // Create an instance of square with color purple
+      const square = new Square("purple");
+
+      // Define the expected SVG markup for a yellow square
+      const incorrectAttributes = `<rect x="50" y="50" width="100" height="100" fill="purple"/>`;
+
+      // Check if the rendered SVG matches the expected SVG
+      expect(square.render()).toBe(incorrectAttributes);
+    });
+  });
 });
