bae841e492a4d54e43d762d8a3c6b93137d71f74
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-22 15:33:11 -0700

    class svg and function writeToFile should not be in the index.js

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const { Circle, Triangle, Square } = require("./lib/shape.js");
 // Questions for user
 const questions = [{}];
 
-class svg {
-  constructor() {}
-  render() {}
-}
-
-// Write README content to a file
-function writeToFile(fileName, data) {
-  fs.writeFile();
-}
-
 // Initialize the process
 function init() {
   inquirer.prompt(questions).then().catch();
